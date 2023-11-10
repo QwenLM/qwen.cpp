d6d22fdc91091fc74046625239fb50de33eed023
xiyihan0 <71264788+xiyihan0@users.noreply.github.com>
2023-11-10 11:11:36 +0800

    Update tiktoken.h
    139行assert中的!=应改为==

--- tiktoken.h
+++ tiktoken.h
@@ -136,7 +136,7 @@ class tiktoken {
 			for (const auto &[k, v] : encoder_) {
 				decoder_.emplace(v, k);
 			}
-			assert(encoder_.size() != decoder_.size() && "Encoder and decoder must be of equal length; maybe you had duplicate token indices in your encoder?");
+			assert(encoder_.size() == decoder_.size() && "Encoder and decoder must be of equal length; maybe you had duplicate token indices in your encoder?");
 
 			for (const auto &[k, v] : special_tokens_encoder) {
 				special_tokens_decoder.emplace(v, k);
